a714fcdd29066cc287ef0efa689f35c0418630d2
Fortin <dunvladyur4@gmail.com>
2017-12-10 00:17:32 +0300

    test db

--- src/srcClass/dataPack/DataForPage.java
+++ src/srcClass/dataPack/DataForPage.java
@@ -2,6 +2,7 @@ package dataPack;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.sql.*;
 
 public class DataForPage{
 
@@ -23,6 +24,56 @@ public class DataForPage{
             file.close();
         } catch (IOException ignored){};
     }*/
+
+    public static Connection conn;
+    public static Statement statmt;
+    public static ResultSet resSet;
+
+    public static String Conn() throws ClassNotFoundException, SQLException
+    {
+        conn = null;
+        Class.forName("org.sqlite.JDBC");
+        conn = DriverManager.getConnection("jdbc:sqlite:TEST1.s3db");statmt = conn.createStatement();
+        statmt.execute("CREATE TABLE if not exists 'users' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'name' text, 'phone' INT);");
+        statmt.execute("INSERT INTO 'users' ('name', 'phone') VALUES ('Petya', 125453); ");
+        statmt.execute("INSERT INTO 'users' ('name', 'phone') VALUES ('Vasya', 321789); ");
+        statmt.execute("INSERT INTO 'users' ('name', 'phone') VALUES ('Masha', 456123); ");
+        resSet = statmt.executeQuery("SELECT * FROM users");
+        if(resSet.next())
+        {
+            String  name = resSet.getString("name");
+            conn.close();
+            statmt.close();
+            resSet.close();
+            return "name = " + name;
+        }else {
+            conn.close();
+            statmt.close();
+            resSet.close();
+            return "bad";
+        }
+    }
+
+    public static String ReadDB() throws ClassNotFoundException, SQLException
+    {
+        Conn();
+        if (conn == null) return "Error";
+        resSet = statmt.executeQuery("SELECT * FROM users");
+
+        if(resSet.next())
+        {
+            int id = resSet.getInt("id");
+            conn.close();
+            statmt.close();
+            resSet.close();
+            return "ID = " + id ;
+        }else {
+            conn.close();
+            statmt.close();
+            resSet.close();
+            return "bad";
+        }
+    }
 
     public static String getDate(){
         Date dateNow = new Date();
